dc7dc65cad48275f200f946ec51a57f0153317b2
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-06-10 10:59:18 -0400

    momentarily commented out auth component

--- front-end/src/auth/Auth.js
+++ front-end/src/auth/Auth.js
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import { auth } from '../config/firebase-config.js';
-import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+// import React, { useState } from 'react';
+// import { auth } from '../config/firebase-config.js';
+// import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
-const Auth = () => {
-  const [user, setUser] = useState(null);
+// const Auth = () => {
+//   const [user, setUser] = useState(null);
 
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        setUser(user);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+//   const signInWithGoogle = () => {
+//     const provider = new GoogleAuthProvider();
+//     signInWithPopup(auth, provider)
+//       .then((result) => {
+//         const user = result.user;
+//         setUser(user);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//       });
+//   };
 
-  return (
-    <div>
-      {user ? (
-        <div>
-          <h1>Welcome</h1>
-        </div>
-      ) : (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {user ? (
+//         <div>
+//           <h1>Welcome</h1>
+//         </div>
+//       ) : (
+//         <button onClick={signInWithGoogle}>Sign in with Google</button>
+//       )}
+//     </div>
+//   );
+// };
 
-export default Auth;
+// export default Auth;
