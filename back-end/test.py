6b9e0df66cd48eb98a6a3aba0dbde821fb80c4dd
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-06-13 23:17:44 -0400

    updated test element

--- back-end/test.py
+++ back-end/test.py
@@ -4,5 +4,8 @@ import os
 genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-response = model.generate_content("Give me the context of Trump's campaign in 2024. Surf the web to give me three links to articles that talk about diverse opinions to Trump's campaign.")
+# Input you test prompt here
+prompt = "How long has it been since June 12, 2024 19:24?"
+
+response = model.generate_content(prompt)
 print(response)
