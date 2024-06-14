d624c67972bef0de58fe10a177727c3443ed2686
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-06-14 12:11:10 -0400

    process publisher

---
+++ back-end/functions/process_publisher.py
@@ -0,0 +1,13 @@
+from talk_to_gemini import talk_to_gemini
+
+def process_publisher(publisher: str) -> str:
+    '''
+    Given a publisher, return some basic information about it, its political leaning, and any red flags.
+    :param publisher: Publisher as a string.
+    :return: Generated caption of the publisher.
+    '''
+    prompt = f"Tell me about what kind of news {publisher} publishes in one sentence. Then tell me in another sentence their political affiliation and if there are any controversies involving them."
+    resp = talk_to_gemini(prompt)
+
+    # TODO: Later integrate with Media Bias Fact Check API (given that we have the funds)
+    return resp
--- back-end/test.py
+++ back-end/test.py
@@ -5,7 +5,7 @@ genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Input you test prompt here
-prompt = "How long has it been since June 12, 2024 19:24?"
+prompt = "Tell me about what kind of news GBH News publishes in one sentence. Then tell me in another sentence their political affiliation and if there are any controversies involving them."
 
 response = model.generate_content(prompt)
 print(response)
