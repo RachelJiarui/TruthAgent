f8bd1207730be711580c87e37be3ab8862b59900
RachelJiarui <81569164+RachelJiarui@users.noreply.github.com>
2024-06-15 09:49:18 -0400

    queried for tab correctly

--- front-end/public/background.js
+++ front-end/public/background.js
@@ -1,12 +1,36 @@
 try {
   chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete") {
+    if (
+      changeInfo.status === "complete" &&
+      tab.url &&
+      !tab.url.startsWith("chrome://")
+    ) {
       chrome.scripting.executeScript({
         files: ["contentScript.js"],
         target: { tabId: tab.id },
       });
     }
   });
+
+  console.log("Did we get here?");
+
+  (async () => {
+    try {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      if (tab && tab.url && !tab.url.startsWith("chrome://")) {
+        console.log("Did we get there?");
+        console.log(tab.url);
+      } else {
+        console.log("Tab is not available or is a chrome:// URL");
+      }
+    } catch (err) {
+      console.error("Error querying the active tab:", err);
+    }
+  })();
 } catch (e) {
   console.log(e);
 }
